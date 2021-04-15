a03a0bca96b4abb5b291e263b8e533f427938b22
guipradods <guipradods@gmail.com>
2021-04-15 16:15:43 -0300

    Mudança na requisição do método de GET para POST

--- src/main/java/com/example/demo/controller/PontoController.java
+++ src/main/java/com/example/demo/controller/PontoController.java
@@ -5,6 +5,7 @@ import com.example.demo.service.PontoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class PontoController {
     @Autowired
     private PontoService pontoService;
 
-    @GetMapping()
+    @PostMapping()
     public ResponseEntity marcarPonto() {
 
         if (pontoService.checarDiaDaSemanaValido(LocalDate.now())) {
